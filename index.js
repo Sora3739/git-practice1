80363c5d55f313b6a4b126d12a6fd5dd606ef5b6
sorasky1309@gmail.com <sorasky1309@gmail.com>
2022-06-10 16:56:09 +0900

    JavaScriptAddTask修正

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function addTask(title) {
     const task = { isDone: false }
 
     // やることのタイトルを設定(このままだと、すべてのタイトルがHello Worldになってしまう！)
-   
-    task.title =document.getElementById("todo-list-form").value;
+  
+    task.title =title;
 
     // 以降は無視して良い
     tasks.push(task);
